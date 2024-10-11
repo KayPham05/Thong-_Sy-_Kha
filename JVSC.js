9a9faa5b75f838ef2526ae90374267ea78022f41
Thong <trantrungthong174@gmail.com>
2024-10-11 16:10:48 +0700

    thongdz

--- JVSC.js
+++ JVSC.js
@@ -1,22 +1,41 @@
+const listImage = document.querySelector('.list-images'); // The container for the images
+const imgs = document.querySelectorAll('.list-images img'); // All images in the slider
+const btnLeft = document.querySelector('.btn-left'); // Previous button
+const btnRight = document.querySelector('.btn-right'); // Next button
 
-const listImage = document.querySelector('.list-images')
-console.log(listImage)
-const imgs = document.getElementsByClassName('imge1')
-const lenght = imgs.length
-let  current=0;
-setInterval(()=>{
-    // if(current == lenght -1){
-    //     current = 0
-    //     let width= imgs[0].offsetWidth
-    //     listImage.style.transform = `translateX(${width * -1 *current})`
-    // }
-    // else{
-    if(current == lenght - 1) {
-        current = 0; 
+let current = 0; // Current image index
+const length = imgs.length; // Total number of images
+
+// Function to reload the slider and update the position
+function reloadSlider() {
+    let width = imgs[0].offsetWidth; // Width of each image
+    listImage.style.transform = `translateX(${-width * current}px)`; // Move the slider
+}
+
+// Next button click event
+btnRight.onclick = function () {
+    if (current + 1 >= length) {
+        current = 0;
+    } else {
+        current++;
     }
-        current++
-        let width = imgs[0].offsetWidth
-        console.log(width)
-        listImage.style.transform = `translateX(${width * -1 *current}px)`
-    // }
-},4000)
+    reloadSlider();
+}
+
+// Previous button click event
+btnLeft.onclick = function () {
+    if (current - 1 < 0) {
+        current = length - 1;
+    } else {
+        current--;
+    }
+    reloadSlider();
+}
+
+// Automatic sliding every 4 seconds
+let refreshSlide = setInterval(() => {
+    btnRight.click(); // Simulate the "next" button being clicked
+}, 4000);
+
+// Initial call to set the slider position
+reloadSlider();
