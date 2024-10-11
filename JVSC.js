86afa8f0611f0f746d872573c78804fcc735d70b
KayPham <kha999888777666@gmail.com>
2024-10-11 15:57:42 +0700

    kha

--- JVSC.js
+++ JVSC.js
@@ -2,21 +2,38 @@
 const listImage = document.querySelector('.list-images')
 console.log(listImage)
 const imgs = document.getElementsByClassName('imge1')
+const btnLeft=document.getElementById('btn-left')
+const btnRight=document.getElementById('btn-right')
 const lenght = imgs.length
 let  current=0;
-setInterval(()=>{
-    // if(current == lenght -1){
-    //     current = 0
-    //     let width= imgs[0].offsetWidth
-    //     listImage.style.transform = `translateX(${width * -1 *current})`
-    // }
-    // else{
-    if(current == lenght - 1) {
-        current = 0; 
+const hendelChangseSlide = () =>{
+    if(current == lenght -1){
+        current = 0
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(0px)`
     }
+    else{
         current++
         let width = imgs[0].offsetWidth
         console.log(width)
         listImage.style.transform = `translateX(${width * -1 *current}px)`
-    // }
-},4000)
+    }
+}
+setInterval(hendelChangseSlide,4000)
+btnRight.addEventListener("click",() =>{
+    hendelChangseSlide()
+})
+
+btnLeft.addEventListener("click",() =>{
+    if(current == 0){
+        current = lenght - 1
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(${width * -1 *current}px)`
+    }
+    else{
+        current++
+        let width = imgs[0].offsetWidth
+        console.log(width)
+        listImage.style.transform = `translateX(${width * -1 *current}px)`
+    }
+})
