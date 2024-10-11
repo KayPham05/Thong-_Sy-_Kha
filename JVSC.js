9aad0ac3b5bb344c98cbf494b4f9375a887a653d
KayPham <kha999888777666@gmail.com>
2024-10-11 16:18:27 +0700

    kha

--- JVSC.js
+++ JVSC.js
@@ -3,7 +3,6 @@ const imgs = document.querySelectorAll('.list-images img'); // All images in the
 const btnLeft = document.querySelector('.btn-left'); // Previous button
 const btnRight = document.querySelector('.btn-right'); // Next button
 
-<<<<<<< HEAD
 let current = 0; // Current image index
 const length = imgs.length; // Total number of images
 
@@ -40,43 +39,3 @@ let refreshSlide = setInterval(() => {
 
 // Initial call to set the slider position
 reloadSlider();
-=======
-const listImage = document.querySelector('.list-images')
-console.log(listImage)
-const imgs = document.getElementsByClassName('imge1')
-const btnLeft=document.getElementById('btn-left')
-const btnRight=document.getElementById('btn-right')
-const lenght = imgs.length
-let  current=0;
-const hendelChangseSlide = () =>{
-    if(current == lenght -1){
-        current = 0
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(0px)`
-    }
-    else{
-        current++
-        let width = imgs[0].offsetWidth
-        console.log(width)
-        listImage.style.transform = `translateX(${width * -1 *current}px)`
-    }
-}
-setInterval(hendelChangseSlide,4000)
-btnRight.addEventListener("click",() =>{
-    hendelChangseSlide()
-})
-
-btnLeft.addEventListener("click",() =>{
-    if(current == 0){
-        current = lenght - 1
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 *current}px)`
-    }
-    else{
-        current++
-        let width = imgs[0].offsetWidth
-        console.log(width)
-        listImage.style.transform = `translateX(${width * -1 *current}px)`
-    }
-})
->>>>>>> d3112f7c1138388111ffdebb8d70fcc99c95f497
